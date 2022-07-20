45f668d408fecd8b0e2900cca3e74c42cc76e8c6
Ian-Woo Kim <ianwookim@gmail.com>
2022-07-20 14:17:00 -0700

    format haskell files

--- app/Main.hs
+++ app/Main.hs
@@ -1,41 +1,46 @@
 {-# LANGUAGE ApplicativeDo #-}
+
 module Main where
 
-import Options.Applicative
 import Data.Aeson as A
-import Data.Traversable (for)
 import Data.ByteString.Lazy as B (readFile, split, toStrict)
-import Data.Text.Encoding (decodeUtf8')
 import Data.Char (ord)
+import Data.Text.Encoding (decodeUtf8')
+import Data.Traversable (for)
 import Lib
+import Options.Applicative
 
 data Opts = Opts
-    { file :: FilePath
-    }
-    deriving Show
+  { file :: FilePath
+  }
+  deriving (Show)
 
 optsParser :: ParserInfo Opts
 optsParser =
-    info (inner <**> helper)
-        (fullDesc
-        <> progDesc "Analyze GHC build output")
-    where
-        inner = do
-            file <- strOption
-                ( long "file"
-                <> metavar "FILE"
-                <> help "Filename to read GHC log from" )
-            return Opts { file }
+  info
+    (inner <**> helper)
+    ( fullDesc
+        <> progDesc "Analyze GHC build output"
+    )
+  where
+    inner = do
+      file <-
+        strOption
+          ( long "file"
+              <> metavar "FILE"
+              <> help "Filename to read GHC log from"
+          )
+      return Opts {file}
 
 main :: IO ()
 main = do
-    opts <- execParser optsParser
+  opts <- execParser optsParser
 
-    logLines <- B.split (fromIntegral . ord $ '\n') <$> B.readFile (file opts)
+  logLines <- B.split (fromIntegral . ord $ '\n') <$> B.readFile (file opts)
 
-    for logLines $ \line -> do
-        case decode line :: Maybe GhcMessage of
-          Just message -> putStrLn $ show message
-          Nothing -> putStrLn $ "Not a JSON message: " ++ (show $ decodeUtf8' $ toStrict line)
+  for logLines $ \line -> do
+    case decode line :: Maybe GhcMessage of
+      Just message -> putStrLn $ show message
+      Nothing -> putStrLn $ "Not a JSON message: " ++ (show $ decodeUtf8' $ toStrict line)
 
-    return ()
+  return ()
