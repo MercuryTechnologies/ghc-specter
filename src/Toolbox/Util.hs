ccecf6293b4c57cf86fc771044d4afb6d30eef3c
Ian-Woo Kim <ianwookim@gmail.com>
2022-07-22 13:31:44 -0700

    Plugin.Trivial trivial driverPlugin

---
+++ src/Toolbox/Util.hs
@@ -0,0 +1,16 @@
+module Toolbox.Util
+  ( showPpr,
+    printPpr,
+  )
+where
+
+import Control.Monad.IO.Class (MonadIO (liftIO))
+import GHC.Driver.Session (DynFlags)
+import GHC.Plugins (showSDoc)
+import GHC.Utils.Outputable (Outputable (ppr))
+
+showPpr :: (Outputable a) => DynFlags -> a -> String
+showPpr dflags = showSDoc dflags . ppr
+
+printPpr :: (Outputable a, MonadIO m) => DynFlags -> a -> m ()
+printPpr dflags = liftIO . putStrLn . showPpr dflags
