becf9fc6f3f3bcd93f649254015c1b501a3d8e98
Ian-Woo Kim <ianwookim@gmail.com>
2022-08-04 13:50:13 -0700

    construct rev dependency map

--- app/toolbox-server/Toolbox/Render/Session.hs
+++ app/toolbox-server/Toolbox/Render/Session.hs
@@ -1,4 +1,4 @@
-{-# OPTIONS_GHC -Werror #-}
+{-# LANGUAGE BangPatterns #-}
 
 module Toolbox.Render.Session
   ( renderSession,
@@ -7,6 +7,9 @@ where
 
 import Concur.Core (Widget)
 import Concur.Replica (div, pre, text)
+import Data.IntMap (IntMap)
+import qualified Data.IntMap as IM
+import Data.List (foldl')
 import Data.Text (Text)
 import qualified Data.Text as T
 import Replica.VDOM.Types (HTML)
@@ -17,13 +20,33 @@ import Toolbox.Channel
 import Toolbox.Server.Types (ServerState (..))
 import Prelude hiding (div)
 
+mkRevDep :: [(Int, [Int])] -> IntMap [Int]
+mkRevDep = foldl' step IM.empty
+  where
+    step !acc (i, js) =
+      foldl' (\(!acc') j -> IM.insertWith (<>) j [i] acc') acc js
+
+analyze :: ModuleGraphInfo -> Text
+analyze graphInfo =
+  let modDep = mginfoModuleDep graphInfo
+      revDepMap = mkRevDep modDep
+      orphans = filter (\(_, js) -> null js) modDep
+      nOrphans = length orphans
+      nSingle = length $ filter (\(_, js) -> length js == 1) modDep
+   in (T.pack $ show nOrphans) <> ", " <> (T.pack $ show nSingle)
+        <> "\n=============\n"
+        <> (T.pack $ show revDepMap)
+
 formatModuleGraphInfo :: ModuleGraphInfo -> Text
 formatModuleGraphInfo mgi =
-  let txt1 =
+  let txt0 = analyze mgi
+      txt1 =
         T.intercalate "\n" . fmap (T.pack . show) $ mginfoModuleNameMap mgi
       txt2 =
         T.intercalate "\n" . fmap (T.pack . show) $ mginfoModuleDep mgi
-   in "(key, module):\n"
+   in txt0
+        <> "\n-----------------\n"
+        <> "(key, module):\n"
         <> txt1
         <> "\n-----------------\n"
         <> "dependencies:\n"
