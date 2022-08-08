671f246b0cda990889e1dd86a92ec6c1888904d2
Ian-Woo Kim <ianwookim@gmail.com>
2022-08-08 14:06:13 -0700

    replace step for clustering

--- app/graph-test/Main.hs
+++ app/graph-test/Main.hs
@@ -11,11 +11,52 @@ import Data.IntMap (IntMap)
 import qualified Data.IntMap as IM
 import qualified Data.List as L
 import Data.Maybe (mapMaybe)
+import Data.Monoid (First (..))
 import Data.Text (Text)
 import qualified Data.Text as T
 import qualified Data.Text.IO as TIO
 import System.IO (IOMode (..), withFile)
 import Toolbox.Channel (ModuleGraphInfo (..))
+
+-- | representative vertex, other vertices that belong to this cluster
+newtype ClusterVertex = Cluster Int --  [Int]
+  deriving (Show)
+
+repCluster :: ClusterVertex -> Int
+repCluster (Cluster v) = v
+
+-- | intermediate cluster vertex
+data ICVertex
+  = Unclustered Int
+  | Clustered ClusterVertex
+  deriving (Show)
+
+initICVertex :: Int -> ICVertex
+initICVertex i = Unclustered i
+
+-- cluster and elements
+type ClusterState = [(ClusterVertex, [Int])]
+
+replaceStep ::
+  ClusterState ->
+  [(ICVertex, ([ICVertex], [ICVertex]))] ->
+  [(ICVertex, ([ICVertex], [ICVertex]))]
+replaceStep clustering gr = fmap replace gr
+  where
+    replaceEach :: ClusterState -> ICVertex -> ICVertex
+    replaceEach clustering c@(Clustered {}) = c
+    replaceEach clustering (Unclustered v) =
+      let match v (cls, vs)
+            | v `elem` vs = First (Just cls)
+            | otherwise = First Nothing
+       in case getFirst (foldMap (match v) clustering) of
+            Nothing -> Unclustered v
+            (Just cls) -> Clustered cls
+    replace (v, (os, is)) =
+      let os' = fmap (replaceEach clustering) os
+          is' = fmap (replaceEach clustering) is
+          v' = replaceEach clustering v
+       in (v', (os', is'))
 
 mkRevDep :: [(Int, [Int])] -> IntMap [Int]
 mkRevDep deps = L.foldl' step emptyMap deps
@@ -27,15 +68,24 @@ mkRevDep deps = L.foldl' step emptyMap deps
 nodeSizeLimit :: Int
 nodeSizeLimit = 150
 
-filterOutSmallNodes :: ModuleGraphInfo -> [Int]
-filterOutSmallNodes graphInfo =
+-- ( vertex, (dep, revdep)) per each item
+getBiDepGraph :: ModuleGraphInfo -> [(Int, ([Int], [Int]))]
+getBiDepGraph graphInfo =
   let modDep = mginfoModuleDep graphInfo
       modRevDepMap = mkRevDep modDep
       modRevDep = IM.toList modRevDepMap
       modBiDep = concat $ inner grouping joiner fst fst modDep modRevDep
         where
           joiner (i, js) (_, ks) = (i, (js, ks))
-   in fmap fst $ filter (\(_, (js, ks)) -> length js + length ks > nodeSizeLimit) modBiDep
+   in modBiDep
+
+filterOutSmallNodes :: ModuleGraphInfo -> [Int]
+filterOutSmallNodes graphInfo =
+  let modBiDep = getBiDepGraph graphInfo
+   in fmap fst $
+        filter
+          (\(_, (js, ks)) -> length js + length ks > nodeSizeLimit)
+          modBiDep
 
 analyze :: ModuleGraphInfo -> Text
 analyze graphInfo =
@@ -70,8 +120,16 @@ analyze graphInfo =
         <> "\n=============\n"
         <> "larges: "
         <> (T.pack $ show largeNames)
+        <> "\n-------------\n"
         <> "# of larges: "
         <> (T.pack $ show (length larges))
+
+-- | (number of vertices, number of edges)
+stat :: ModuleGraphInfo -> (Int, Int)
+stat mgi =
+  let nVtx = length $ mginfoModuleNameMap mgi
+      nEdg = sum $ fmap (length . snd) $ mginfoModuleDep mgi
+   in (nVtx, nEdg)
 
 formatModuleGraphInfo :: ModuleGraphInfo -> Text
 formatModuleGraphInfo mgi =
@@ -81,6 +139,7 @@ formatModuleGraphInfo mgi =
         T.intercalate "\n" . fmap (T.pack . show) $ mginfoModuleDep mgi
       txt3 =
         T.pack . show $ mginfoModuleTopSorted mgi
+      (nVtx, nEdg) = stat mgi
    in "(key, module):\n"
         <> txt1
         <> "\n-----------------\n"
@@ -91,6 +150,11 @@ formatModuleGraphInfo mgi =
         <> txt3
         <> "\n=================\n"
         <> analyze mgi
+        <> "\n=================\n"
+        <> "# of vertices: "
+        <> T.pack (show nVtx)
+        <> ", # of edges: "
+        <> T.pack (show nEdg)
 
 main :: IO ()
 main =
@@ -98,5 +162,15 @@ main =
     lbs <- BL.hGetContents h
     case eitherDecode @ModuleGraphInfo lbs of
       Left e -> print e
-      Right mgi ->
-        TIO.putStrLn (formatModuleGraphInfo mgi)
+      Right mgi -> do
+        let gr = mginfoModuleDep mgi
+            bgr =
+              fmap (\(v, (os, is)) -> (initICVertex v, (fmap initICVertex os, fmap initICVertex is))) $
+                getBiDepGraph mgi
+            seeds =
+              fmap (\i -> (Cluster i, [i])) $ filterOutSmallNodes mgi
+            clustered = replaceStep seeds bgr
+        print seeds
+        print clustered
+        --
+        print (fmap fst clustered)
