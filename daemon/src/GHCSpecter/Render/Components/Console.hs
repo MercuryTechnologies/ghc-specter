77328be9b30dae61ff8017eed1bc425fcca7c4ea
Ian-Woo Kim <ianwookim@gmail.com>
2022-10-04 20:20:31 -0700

    hacky automatic scrolldown solution!

--- daemon/src/GHCSpecter/Render/Components/Console.hs
+++ daemon/src/GHCSpecter/Render/Components/Console.hs
@@ -6,6 +6,7 @@ where
 import Concur.Core (Widget)
 import Concur.Replica
   ( classList,
+    id,
     onClick,
     onInput,
     onKeyPress,
@@ -22,6 +23,7 @@ import GHCSpecter.UI.ConcurReplica.DOM
     input,
     nav,
     pre,
+    script,
     text,
   )
 import GHCSpecter.UI.ConcurReplica.Types (IHTML)
@@ -31,7 +33,7 @@ import GHCSpecter.Util.Map
     KeyMap,
     lookupKey,
   )
-import Prelude hiding (div)
+import Prelude hiding (div, id)
 
 render ::
   (IsKey k, Eq k) =>
@@ -62,13 +64,33 @@ render tabs contents mfocus inputEntry = div [] [consoleTabs, console]
       let mtxt = do
             focus <- mfocus
             lookupKey focus contents
+
+          -- This is a hack. Property update should be supported by concur-replica.
+          -- TODO: implement prop update in internalized concur-replica.
+          scriptContent =
+            script
+              []
+              [ text
+                  "var me = document.currentScript;\n\
+                  \var myParent = me.parentElement;\n\
+                  \console.log(myParent);\n\
+                  \var config = {attirbutes: true, childList: true, subtree: true, characterData: true };\n\
+                  \var callback = (mutationList, observer) => {\n\
+                  \      myParent.scrollTop = myParent.scrollHeight;\n\
+                  \    };\n\
+                  \var observer = new MutationObserver(callback);\n\
+                  \observer.observe(myParent, config);\n"
+              ]
        in pre
-            [ style
+            [ id "console"
+            , style
                 [ ("height", "200px")
                 , ("overflow", "scroll")
                 ]
             ]
-            [text (fromMaybe "" mtxt)]
+            [ scriptContent
+            , text (fromMaybe "" mtxt)
+            ]
     consoleInput =
       divClass
         "console-input"
@@ -82,8 +104,4 @@ render tabs contents mfocus inputEntry = div [] [consoleTabs, console]
             , textProp "value" inputEntry
             ]
         ]
-    console =
-      divClass
-        "console"
-        []
-        [consoleContent, consoleInput]
+    console = divClass "console" [] [consoleContent, consoleInput]
