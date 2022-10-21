26b5b11b4d64b5f125b5c2ab8e5c01c8a3f61d5f
Ian-Woo Kim <ianwookim@gmail.com>
2022-10-21 14:52:38 -0700

    dev shell for GHC 9.2 and GHC-9.4

--- flake.nix
+++ flake.nix
@@ -1,12 +1,9 @@
 {
   description = "ghc-build-analzyer: utility for analyzing GHC build";
   inputs = {
-    nixpkgs = {
-      type = "github";
-      owner = "avieth";
-      repo = "nixpkgs";
-      rev = "346134b9b036a6f5c3f7239b450343c88954acea";
-    };
+    # nixpkgs/master on 2022-10-21
+    nixpkgs.url =
+      "github:NixOS/nixpkgs/71c5816834f93840dd301ec384c9d7947e97c27d";
     flake-utils.url = "github:numtide/flake-utils";
     concur = {
       url = "github:wavewave/concur/ghc-9.2";
@@ -21,12 +18,12 @@
       flake = false;
     };
     fficxx = {
-      url = "github:wavewave/fficxx/aarch64-darwin";
+      url = "github:wavewave/fficxx/master";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
     };
     hs-ogdf = {
-      url = "github:wavewave/hs-ogdf/aarch64-darwin";
+      url = "github:wavewave/hs-ogdf/master";
       inputs.nixpkgs.follows = "nixpkgs";
       inputs.flake-utils.follows = "flake-utils";
       inputs.fficxx.follows = "fficxx";
@@ -40,48 +37,59 @@
           haskellPackages = prev.haskell.packages.ghc924;
         };
         pkgs = import nixpkgs {
-          overlays = [
-            overlayGHC
-            (fficxx.overlay.${system})
-            (hs-ogdf.overlay.${system})
-          ];
+          #overlays = [
+          #  overlayGHC
+          #  (fficxx.overlay.${system})
+          #  (hs-ogdf.overlay.${system})
+          #];
           inherit system;
-          config.allowBroken = true;
+          #config.allowBroken = true;
         };
-        hspkgs = pkgs.haskellPackages.override (old: {
-          overrides = pkgs.lib.composeExtensions (old.overrides or (_: _: { }))
-            (self: super: {
-              "criterion" = pkgs.haskell.lib.dontCheck super.criterion;
-              "concur-core" =
-                self.callCabal2nix "concur-core" (concur + "/concur-core") { };
-              "concur-replica" =
-                self.callCabal2nix "concur-replica" concur-replica { };
-              "http2" = pkgs.haskell.lib.dontCheck super.http2;
-              "replica" = self.callCabal2nix "replica" replica { };
-              "retry" = pkgs.haskell.lib.dontCheck super.retry;
-            });
-        });
-        hsenv = hspkgs.ghcWithPackages (p: [
-          p.aeson
-          p.binary-instances
-          p.cabal-install
-          p.concur-core
-          p.concur-replica
-          p.discrimination
-          p.extra
-          p.fourmolu
-          p.hiedb
-          p.hpack
-          p.hspec
-          p.lens
-          p.OGDF
-          p.optparse-applicative
-          p.replica
-          p.socket
-          p.text
-          p.time
-        ]);
+
+        haskellOverlay = final: hself: hsuper: {
+          "criterion" = final.haskell.lib.dontCheck hsuper.criterion;
+          "concur-core" =
+            hself.callCabal2nix "concur-core" (concur + "/concur-core") { };
+          "concur-replica" =
+            hself.callCabal2nix "concur-replica" concur-replica { };
+          "http2" = final.haskell.lib.dontCheck hsuper.http2;
+          "replica" = hself.callCabal2nix "replica" replica { };
+          "retry" = final.haskell.lib.dontCheck hsuper.retry;
+        };
+
+        hpkgsFor = compiler:
+          pkgs.haskell.packages.${compiler}.extend (hself: hsuper:
+            (fficxx.haskellOverlay.${system} pkgs hself hsuper)
+            // (hs-ogdf.haskellOverlay.${system} pkgs hself hsuper)
+            // (haskellOverlay pkgs hself hsuper));
+
+        mkShellFor = compiler:
+          let
+            hsenv = (hpkgsFor compiler).ghcWithPackages (p: [
+              p.aeson
+              p.binary-instances
+              p.cabal-install
+              p.concur-core
+              p.concur-replica
+              p.discrimination
+              p.extra
+              p.fourmolu
+              p.hiedb
+              p.hpack
+              p.hspec
+              p.lens
+              p.OGDF
+              p.optparse-applicative
+              p.replica
+              p.socket
+              p.text
+              p.time
+            ]);
+          in pkgs.mkShell { packages = [ hsenv pkgs.nixfmt ]; };
+
+        supportedCompilers = [ "ghc924" "ghc942" ];
       in {
-        devShells.default = pkgs.mkShell { packages = [ hsenv pkgs.nixfmt ]; };
+        inherit haskellOverlay;
+        devShells = pkgs.lib.genAttrs supportedCompilers mkShellFor;
       });
 }
