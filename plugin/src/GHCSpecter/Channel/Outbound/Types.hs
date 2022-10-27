590e1638712c4f797d1cedaecab1550ce78d0a77
Ian-Woo Kim <ianwookim@gmail.com>
2022-10-27 02:10:29 -0700

    move orphan instances

--- plugin/src/GHCSpecter/Channel/Outbound/Types.hs
+++ plugin/src/GHCSpecter/Channel/Outbound/Types.hs
@@ -1,7 +1,5 @@
 {-# LANGUAGE DataKinds #-}
 {-# LANGUAGE GADTs #-}
-{-# LANGUAGE MultiWayIf #-}
-{-# OPTIONS_GHC -fno-warn-orphans #-}
 
 module GHCSpecter.Channel.Outbound.Types
   ( -- * information types
@@ -27,7 +25,6 @@ module GHCSpecter.Channel.Outbound.Types
 where
 
 import Data.Aeson (FromJSON (..), ToJSON (..))
-import Data.Aeson qualified as A
 import Data.Binary (Binary (..))
 import Data.Binary.Instances.Time ()
 import Data.IntMap (IntMap)
@@ -38,27 +35,12 @@ import Data.Text (Text)
 import Data.Time.Clock (UTCTime)
 import Data.Tree (Forest)
 import GHC.Generics (Generic)
-import GHC.RTS.Flags
-  ( CCFlags,
-    ConcFlags,
-    DebugFlags,
-    DoCostCentres,
-    DoHeapProfile,
-    DoTrace,
-    GCFlags,
-    GiveGCStats,
-    IoSubSystem (..),
-    MiscFlags,
-    ParFlags,
-    ProfFlags,
-    RTSFlags,
-    TickyFlags,
-    TraceFlags,
-  )
+import GHC.RTS.Flags (RTSFlags)
 import GHCSpecter.Channel.Common.Types
   ( DriverId (..),
     type ModuleName,
   )
+import GHCSpecter.Data.GHC.Orphans ()
 
 data Channel
   = CheckImports
@@ -162,109 +144,6 @@ instance Binary ModuleGraphInfo
 
 emptyModuleGraphInfo :: ModuleGraphInfo
 emptyModuleGraphInfo = ModuleGraphInfo mempty mempty []
-
--- orphan instances
-instance Binary GiveGCStats
-
-instance FromJSON GiveGCStats
-
-instance ToJSON GiveGCStats
-
-instance Binary GCFlags
-
-instance FromJSON GCFlags
-
-instance ToJSON GCFlags
-
-instance Binary CCFlags
-
-instance FromJSON CCFlags
-
-instance ToJSON CCFlags
-
-instance Binary ConcFlags
-
-instance FromJSON ConcFlags
-
-instance ToJSON ConcFlags
-
-instance Binary DebugFlags
-
-instance FromJSON DebugFlags
-
-instance ToJSON DebugFlags
-
-instance Binary DoCostCentres
-
-instance FromJSON DoCostCentres
-
-instance ToJSON DoCostCentres
-
-instance Binary DoHeapProfile
-
-instance FromJSON DoHeapProfile
-
-instance ToJSON DoHeapProfile
-
-instance Binary DoTrace
-
-instance FromJSON DoTrace
-
-instance ToJSON DoTrace
-
-instance Generic IoSubSystem
-
-instance Binary IoSubSystem where
-  put = put . fromEnum
-  get = toEnum <$> get
-
-instance FromJSON IoSubSystem where
-  parseJSON =
-    A.withText "IoSubSystem" $ \txt ->
-      if | txt == "IoPOSIX" -> pure IoPOSIX
-         | txt == "IoNative" -> pure IoNative
-         | otherwise -> fail "cannot parse IoSubSystem"
-
-instance ToJSON IoSubSystem where
-  toJSON IoPOSIX = A.String "IoPOSIX"
-  toJSON IoNative = A.String "IoNative"
-
-instance Binary MiscFlags
-
-instance FromJSON MiscFlags
-
-instance ToJSON MiscFlags
-
-instance Binary ParFlags
-
-instance FromJSON ParFlags
-
-instance ToJSON ParFlags
-
-instance Binary ProfFlags
-
-instance FromJSON ProfFlags
-
-instance ToJSON ProfFlags
-
-instance Binary TickyFlags
-
-instance FromJSON TickyFlags
-
-instance ToJSON TickyFlags
-
-instance Binary TraceFlags
-
-instance FromJSON TraceFlags
-
-instance ToJSON TraceFlags
-
-instance Binary RTSFlags
-
-instance FromJSON RTSFlags
-
-instance ToJSON RTSFlags where
-  toJSON = A.genericToJSON A.defaultOptions
 
 -- | GHC process info, including process id, command line arguments.
 data ProcessInfo = ProcessInfo
