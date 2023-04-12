6efeef14bf8e047320c0b068346d72baecdf7187
Ian-Woo Kim <ianwookim@gmail.com>
2023-04-11 22:16:14 -0700

    Source text view

--- daemon-exp/app/ghc-specter-daemon-exp/SourceView.hs
+++ daemon-exp/app/ghc-specter-daemon-exp/SourceView.hs
@@ -3,18 +3,32 @@
 module SourceView (renderSourceView) where
 
 import Control.Concurrent.STM (TVar)
+import Control.Lens (at, (^.), (^?), _Just)
 import Data.Foldable (for_)
 import Data.Map qualified as Map
-import GHCSpecter.Graphics.DSL (Scene (..))
+import Data.Text.IO qualified as T
+import GHCSpecter.Data.GHC.Hie (
+  HasDeclRow' (..),
+  HasModuleHieInfo (..),
+  ModuleHieInfo,
+ )
+import GHCSpecter.Graphics.DSL (Scene (..), ViewPort (..))
 import GHCSpecter.Render.Components.ModuleTree (compileModuleTree)
 import GHCSpecter.Render.Components.Tab (compileTab)
-import GHCSpecter.Server.Types (ServerState)
+import GHCSpecter.Render.Components.TextView (compileTextView)
+import GHCSpecter.Server.Types (
+  HasHieState (..),
+  HasServerState (..),
+  ServerState,
+ )
 import GHCSpecter.UI.Types (
+  HasSourceViewUI (..),
   SourceViewUI,
   UIState,
  )
 import GHCSpecter.UI.Types.Event (Tab (..))
 import GHCSpecter.Util.Transformation (translateToOrigin)
+import GHCSpecter.Worker.CallGraph (getReducedTopLevelDecls)
 import GI.Cairo.Render qualified as R
 import Renderer (
   addEventMap,
@@ -39,12 +53,38 @@ renderSourceView uiRef vb srcUI ss = do
             }
     renderScene vb sceneTab'
     R.liftIO $ addEventMap uiRef sceneTab
+  -- module tree pane
   for_ (Map.lookup "module-tree" wcfg) $ \vpCvs -> do
-    let scene = compileModuleTree srcUI ss
-        scene' =
-          scene
+    let sceneModTree = compileModuleTree srcUI ss
+        sceneModTree' =
+          sceneModTree
             { sceneGlobalViewPort = vpCvs
             , sceneLocalViewPort = translateToOrigin vpCvs
             }
-    renderScene vb scene'
-    R.liftIO $ addEventMap uiRef scene'
+    renderScene vb sceneModTree'
+    R.liftIO $ addEventMap uiRef sceneModTree'
+  -- source text view
+  for_ (Map.lookup "source-view" wcfg) $ \vpCvs -> do
+    let ViewPort (cx0, cy0) (_cx1, cy1) = vpCvs
+    -- vertical separator
+    R.setSourceRGBA 0 0 0 1
+    R.setLineWidth 1.0
+    R.moveTo cx0 cy0
+    R.lineTo cx0 cy1
+    R.stroke
+    -- source text
+    let hie = ss ^. serverHieState
+        mexpandedModu = srcUI ^. srcViewExpandedModule
+    for_ mexpandedModu $ \modu -> do
+      let mmodHieInfo = hie ^? hieModuleMap . at modu . _Just
+      for_ mmodHieInfo $ \modHieInfo -> do
+        let topLevelDecls = getReducedTopLevelDecls modHieInfo
+            src = modHieInfo ^. modHieSource
+        let sceneSrcView = compileTextView False src []
+            sceneSrcView' =
+              sceneSrcView
+                { sceneGlobalViewPort = vpCvs
+                , sceneLocalViewPort = translateToOrigin vpCvs
+                }
+        renderScene vb sceneSrcView'
+        R.liftIO $ addEventMap uiRef sceneSrcView'
--- daemon/src/GHCSpecter/Render/Components/TextView.hs
+++ daemon/src/GHCSpecter/Render/Components/TextView.hs
@@ -10,6 +10,9 @@ module GHCSpecter.Render.Components.TextView (
   leftOfBox,
   rightOfBox,
 
+  -- * compile
+  compileTextView,
+
   -- * top-level render function
   render,
 ) where
@@ -22,6 +25,13 @@ import Concur.Replica (
 import Concur.Replica.SVG.Props qualified as SP
 import Data.Text (Text)
 import Data.Text qualified as T
+import GHCSpecter.Graphics.DSL (
+  Color (..),
+  Primitive (..),
+  Scene (..),
+  TextPosition (..),
+  ViewPort (..),
+ )
 import GHCSpecter.Render.Util (xmlns)
 import GHCSpecter.UI.ConcurReplica.DOM (text)
 import GHCSpecter.UI.ConcurReplica.SVG qualified as S
@@ -47,6 +57,79 @@ leftOfBox j = charSize * fromIntegral (j - 1)
 
 rightOfBox :: Int -> Double
 rightOfBox j = charSize * fromIntegral j
+
+compileTextView :: Bool -> Text -> [((Int, Int), (Int, Int))] -> Scene
+compileTextView showCharBox txt highlighted =
+  Scene
+    { sceneId = "text-view"
+    , sceneGlobalViewPort = ViewPort (0, 0) (totalWidth, fromIntegral nTotal * rowSize)
+    , sceneLocalViewPort = ViewPort (0, 0) (totalWidth, fromIntegral nTotal * rowSize)
+    , sceneElements = contents
+    }
+  where
+    -- NOTE: Rows and columns are 1-based following the GHC convention.
+    ls :: [(Int, Text)]
+    ls = zip [1 ..] $ T.lines txt
+    rowColChars = [((i, j), c) | (i, t) <- ls, let jcs = zip [1 ..] (T.unpack t), (j, c) <- jcs]
+    nTotal = length ls
+    totalWidth =
+      case ls of
+        [] -> 200
+        _ -> charSize * fromIntegral (maximum $ fmap (T.length . snd) ls)
+    {-
+    charBox ((i, j), _) =
+      S.rect
+        [ SP.x (packShow $ leftOfBox j)
+        , SP.y (packShow $ topOfBox i)
+        , SP.width (packShow charSize)
+        , SP.height (packShow rowSize)
+        , SP.stroke "gray"
+        , SP.strokeWidth "0.25"
+        , SP.fill "none"
+        ]
+        []
+    -}
+    boxSize ((startI, startJ), (endI, endJ)) =
+      let w1 = charSize * fromIntegral (endJ - startJ + 1)
+          h1 = rowSize * fromIntegral (endI - startI + 1)
+       in (w1, h1 + 2)
+    {-
+    highlightBox range@((startI, startJ), _) =
+      S.rect
+        [ SP.x (packShow $ leftOfBox startJ)
+        , SP.y (packShow $ topOfBox startI)
+        , SP.width (packShow $ fst (boxSize range))
+        , SP.height (packShow $ snd (boxSize range))
+        , SP.fill "yellow"
+        ]
+        []
+    highlightBox2 range@((startI, startJ), _) =
+      S.rect
+        [ SP.x (packShow $ leftOfBox startJ)
+        , SP.y (packShow $ topOfBox startI)
+        , SP.width (packShow $ fst (boxSize range))
+        , SP.height (packShow $ snd (boxSize range))
+        , SP.stroke "red"
+        , SP.strokeWidth "1px"
+        , SP.fill "none"
+        ]
+        []
+    -}
+    mkText (i, txt) =
+      DrawText (leftOfBox 1, bottomOfBox i) LowerLeft Black 8 txt
+
+    contents =
+      let contents_ =
+            -- fmap highlightBox highlighted
+            --   ++
+            fmap mkText ls
+       in --   ++ fmap highlightBox2 highlighted
+          contents_
+
+{- if showCharBox
+  then fmap charBox rowColChars ++ contents_
+  else contents_
+-}
 
 render :: Bool -> Text -> [((Int, Int), (Int, Int))] -> Widget IHTML a
 render showCharBox txt highlighted =
