ee8898844969216a942600dc940bc578d98c51af
Ian-Woo Kim <ianwookim@gmail.com>
2023-04-25 18:07:00 -0700

    handle zoomed-out case correctly

--- daemon/src/GHCSpecter/Util/Transformation.hs
+++ daemon/src/GHCSpecter/Util/Transformation.hs
@@ -32,7 +32,6 @@ transformScroll ::
 transformScroll vpLimit dir scale (dx, dy) vp = vp'
   where
     ViewPort (x0, y0) (x1, y1) = vp
-    -- ViewPort (x00, y00) (x01, y01) = vpLimit
     dx' = dx / scale
     dy' = dy / scale
 
@@ -42,25 +41,25 @@ transformScroll vpLimit dir scale (dx, dy) vp = vp'
          in case vpLimit of
               Nothing -> vp'
               Just (ViewPort (x00, y00) (x01, y01)) ->
-                if x1 + dx' < x01 then vp' else vp
+                if x0 + dx' < x00 || x1 + dx' < x01 then vp' else vp
       ScrollDirectionLeft ->
         let vp' = ViewPort (x0 - dx', y0) (x1 - dx', y1)
          in case vpLimit of
               Nothing -> vp'
               Just (ViewPort (x00, y00) (x01, y01)) ->
-                if x0 - dx' > x00 then vp' else vp
+                if x1 - dx' > x01 || x0 - dx' > x00 then vp' else vp
       ScrollDirectionDown ->
         let vp' = ViewPort (x0, y0 + dy') (x1, y1 + dy')
          in case vpLimit of
               Nothing -> vp'
               Just (ViewPort (x00, y00) (x01, y01)) ->
-                if y1 + dy' < y01 then vp' else vp
+                if y0 + dy' < y00 || y1 + dy' < y01 then vp' else vp
       ScrollDirectionUp ->
         let vp' = ViewPort (x0, y0 - dy') (x1, y1 - dy')
          in case vpLimit of
               Nothing -> vp'
               Just (ViewPort (x00, y00) (x01, y01)) ->
-                if y0 - dy' > y00 then vp' else vp
+                if y1 - dy' > y01 || y0 - dy' > y00 then vp' else vp
 
 -- | zoom
 transformZoom ::
