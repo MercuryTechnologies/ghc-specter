40dca87f8e9ee98b0b17b083508c8d115ac107a4
Ian-Woo Kim <ianwookim@gmail.com>
2023-05-10 09:52:02 -0700

    better viewport placement condition

--- daemon/src/GHCSpecter/Util/Transformation.hs
+++ daemon/src/GHCSpecter/Util/Transformation.hs
@@ -12,6 +12,7 @@ module GHCSpecter.Util.Transformation (
 ) where
 
 import Data.List qualified as L
+import Debug.Trace (trace)
 import GHCSpecter.Graphics.DSL (
   EventMap,
   HitEvent,
@@ -26,6 +27,18 @@ import GHCSpecter.UI.Types.Event (ScrollDirection (..))
 isValid :: ViewPort -> Bool
 isValid (ViewPort (x0, y0) (x1, y1)) = x0 <= x1 && y0 <= y1
 
+-- either one of the ranges are completely inside the other
+isOneRangeEmbeddedInTheOther :: (Double, Double) -> (Double, Double) -> Bool
+isOneRangeEmbeddedInTheOther (x0, x1) (x0', x1') =
+  -- (x0, x1) includes (x0', x1')
+  ((x0 <= x0') && (x1 >= x1'))
+    -- (x0', x1') includes (x0, x1)
+    || ((x0' <= x0) && (x1' >= x1))
+
+isOneViewPortEmbeddedInTheOther :: ViewPort -> ViewPort -> Bool
+isOneViewPortEmbeddedInTheOther (ViewPort (x0, y0) (x1, y1)) (ViewPort (x0', y0') (x1', y1')) =
+  isOneRangeEmbeddedInTheOther (x0, x1) (x0', x1') && isOneRangeEmbeddedInTheOther (y0, y1) (y0', y1')
+
 -- | scroll
 transformScroll ::
   Maybe ViewPort ->
@@ -34,45 +47,29 @@ transformScroll ::
   (Double, Double) ->
   ViewPort ->
   ViewPort
-transformScroll vpLimit dir scale (dx, dy) vp = vp''
+transformScroll mvpLimit dir scale (dx, dy) vp = vp''
   where
     ViewPort (x0, y0) (x1, y1) = vp
     dx' = dx / scale
     dy' = dy / scale
 
-    vp'' = case dir of
+    vp' = case dir of
       ScrollDirectionRight ->
-        let vp' = ViewPort (x0 + dx', y0) (x1 + dx', y1)
-         in case vpLimit of
-              Nothing -> vp'
-              Just (ViewPort (x00, _y00) (x01, _y01)) ->
-                if x0 + dx' < x00 || x1 + dx' < x01 then vp' else vp
+        ViewPort (x0 + dx', y0) (x1 + dx', y1)
       ScrollDirectionLeft ->
-        let vp' = ViewPort (x0 - dx', y0) (x1 - dx', y1)
-         in case vpLimit of
-              Nothing -> vp'
-              Just (ViewPort (x00, _y00) (x01, _y01)) ->
-                if x1 - dx' > x01 || x0 - dx' > x00 then vp' else vp
+        ViewPort (x0 - dx', y0) (x1 - dx', y1)
       ScrollDirectionDown ->
-        let vp' = ViewPort (x0, y0 + dy') (x1, y1 + dy')
-         in case vpLimit of
-              Nothing -> vp'
-              Just (ViewPort (_x00, y00) (_x01, y01)) ->
-                if y0 + dy' < y00 || y1 + dy' < y01 then vp' else vp
+        ViewPort (x0, y0 + dy') (x1, y1 + dy')
       ScrollDirectionUp ->
-        let vp' = ViewPort (x0, y0 - dy') (x1, y1 - dy')
-         in case vpLimit of
-              Nothing -> vp'
-              Just (ViewPort (_x00, y00) (_x01, y01)) ->
-                if y1 - dy' > y01 || y0 - dy' > y00 then vp' else vp
+        ViewPort (x0, y0 - dy') (x1, y1 - dy')
       ScrollDirectionSmooth ->
-        let vp' = ViewPort (x0 + dx', y0 + dy') (x1 + dx', y1 + dy')
-         in case vpLimit of
-              Nothing -> vp'
-              Just (ViewPort (x00, y00) (x01, y01)) ->
-                if x0 + dx' < x00 || x1 + dx' < x01 || y0 + dy' < y00 || y1 + dy' < y01
-                  then vp'
-                  else vp
+        ViewPort (x0 + dx', y0 + dy') (x1 + dx', y1 + dy')
+    vp'' = case mvpLimit of
+      Just vpLimit
+        | isOneViewPortEmbeddedInTheOther vpLimit vp && isOneViewPortEmbeddedInTheOther vpLimit vp' -> vp'
+        | isOneViewPortEmbeddedInTheOther vpLimit vp -> vp
+        | otherwise -> vp'
+      Nothing -> vp'
 
 -- | zoom
 transformZoom ::
