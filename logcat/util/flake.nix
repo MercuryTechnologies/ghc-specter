19cb17618daa9b2ad0f89cb2339be04ba980dbe8
Ian-Woo Kim <ianwookim@gmail.com>
2023-06-21 18:50:57 -0700

    eventlog2html subclone to handle large eventlog data  (#193)
    * eventlog2html subclone to handle large eventlog data
    
    Html generated by eventlog2html by large eventlog data with info table is
    corrupted due to stack overflow in javascript. This adds a simple utility
    to display the content. Used for profiling GHC.
    
    * remove irrelevant HROOT deps.
    * format
    * format nix

---
+++ logcat/util/flake.nix
@@ -0,0 +1,59 @@
+{
+  description = "shell for logcat/util";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = import nixpkgs {inherit system;};
+      fontconf = pkgs.makeFontsConf {fontDirectories = [pkgs.freefont_ttf];};
+      overrideCabal = pkgs.haskell.lib.overrideCabal;
+      hpkgs = pkgs.haskell.packages.ghc925.extend (hself: hsuper: {
+        "ormolu" =
+          overrideCabal hsuper.ormolu {enableSeparateBinOutput = false;};
+      });
+
+      hsenv = hpkgs.ghcWithPackages (p: [
+        p.aeson
+        p.directory
+        p.filepath
+        p.gi-cairo
+        p.gi-cairo-connector
+        p.gi-cairo-render
+        p.gi-gdk
+        p.gi-gtk
+        p.gi-gtk-hs
+        p.gi-pango
+        p.gi-pangocairo
+        #p.haskell-gi-base
+        p.lens
+        p.lens-aeson
+        p.pretty-simple
+        p.tagsoup
+        p.text
+        p.time
+        p.vector
+      ]);
+    in {
+      devShells.default = pkgs.mkShell {
+        name = "testshell";
+        buildInputs = [
+          hsenv
+          pkgs.gh
+          pkgs.jq
+          hpkgs.ormolu
+          pkgs.root
+          pkgs.alejandra
+        ];
+        shellHook = ''
+          export MACOSX_DEPLOYMENT_TARGET="10.16"
+          export FONTCONFIG_FILE="${fontconf}"
+          export PANGOCAIRO_BACKEND=fc
+          export PS1="\n[logcat/util:\w]$ \0"
+        '';
+      };
+    });
+}
