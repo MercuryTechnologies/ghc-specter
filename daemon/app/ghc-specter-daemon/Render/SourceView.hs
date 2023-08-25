f3e1828629c6a97e40f9af6489c2e06bedf17fa5
Ian-Woo Kim <ianwookim@gmail.com>
2023-08-25 10:48:26 -0700

    source text view

--- daemon/app/ghc-specter-daemon/Render/SourceView.hs
+++ daemon/app/ghc-specter-daemon/Render/SourceView.hs
@@ -9,17 +9,26 @@ where
 import Control.Monad.Extra (whenM)
 import Control.Monad.IO.Class (liftIO)
 import Control.Monad.Trans.Reader (ReaderT)
+import Data.Foldable (for_)
+import Data.Map qualified as M
 import Foreign.C.String (CString)
 import Foreign.Marshal.Utils (fromBool, toBool)
-import GHCSpecter.Server.Types (ServerState (..))
+import GHCSpecter.Data.GHC.Hie (ModuleHieInfo (..))
+import GHCSpecter.Graphics.DSL (Scene (..))
+import GHCSpecter.Server.Types
+  ( HieState (..),
+    ServerState (..),
+  )
 import GHCSpecter.UI.Components.ModuleTree (buildModuleTree)
--- import GHCSpecter.UI.Components.TextView (buildTextView)
+import GHCSpecter.UI.Components.TextView (buildTextView)
 -- import GHCSpecter.UI.SourceView (buildSuppViewPanel)
 import GHCSpecter.UI.Types
-  ( UIModel (..),
+  ( SourceViewUI (..),
+    UIModel (..),
     UIState (..),
   )
 import GHCSpecter.UI.Types.Event (UserEvent (..))
+import GHCSpecter.Worker.CallGraph (getReducedTopLevelDecls)
 import ImGui qualified
 import Render.Common (renderComponent)
 import STD.Deletable (delete)
@@ -40,14 +49,12 @@ render ui ss = do
     liftIO $ ImGui.tableNextRow 0
     liftIO $ ImGui.tableSetColumnIndex 0
     _ <- liftIO $ ImGui.beginChild ("#session-info" :: CString) vec1 (fromBool False) windowFlagsScroll
-    -- renderSessionInfo ss
-    -- buildModuleTree srcUI ss
     renderModuleTree ui ss
     liftIO ImGui.endChild
     --
     liftIO $ ImGui.tableSetColumnIndex 1
     _ <- liftIO $ ImGui.beginChild ("#process-info" :: CString) vec2 (fromBool False) windowFlagsScroll
-    -- renderProcessPanel ss
+    renderSourceTextView ui ss
     liftIO ImGui.endChild
     --
     liftIO $ ImGui.tableSetColumnIndex 2
@@ -68,3 +75,34 @@ renderModuleTree ui ss = do
       renderComponent SourceViewEv (buildModuleTree srcUI ss)
   where
     srcUI = ui._uiModel._modelSourceView
+
+renderSourceTextView :: UIState -> ServerState -> ReaderT (SharedState UserEvent) IO ()
+renderSourceTextView ui ss = do
+  let hie = ss._serverHieState
+      mexpandedModu = srcUI._srcViewExpandedModule
+  for_ mexpandedModu $ \modu -> do
+    let mmodHieInfo = M.lookup modu (hie._hieModuleMap)
+    for_ mmodHieInfo $ \modHieInfo -> do
+      let topLevelDecls = getReducedTopLevelDecls modHieInfo
+          src = modHieInfo._modHieSource
+      renderState <- mkRenderState
+      liftIO $
+        runImRender renderState $
+          renderComponent
+            SourceViewEv
+            (do
+                scene <- buildTextView src (fmap fst topLevelDecls)
+                pure scene {sceneId = "source-view"}
+            )
+  where
+    srcUI = ui._uiModel._modelSourceView
+
+{-
+      sceneSrcView <- buildTextView src (fmap fst topLevelDecls)
+      let sceneSrcView' =
+            sceneSrcView
+              { sceneId = "source-view",
+                sceneGlobalViewPort = sceneGlobalViewPort scene0,
+                sceneLocalViewPort = sceneLocalViewPort scene0
+              }
+-}
