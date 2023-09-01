8b703e7d4e57c820bf081c6229d814ea25ecb40c
Ian-Woo Kim <ianwookim@gmail.com>
2023-08-31 22:25:22 -0700

    first interpreting haskell expr inside plugin

--- plugin/src/Plugin/GHCSpecter/Tasks/Common.hs
+++ plugin/src/Plugin/GHCSpecter/Tasks/Common.hs
@@ -5,21 +5,26 @@ module Plugin.GHCSpecter.Tasks.Common
   )
 where
 
+import Data.Text (Text)
 import GHC.Driver.Env (runInteractiveHsc)
 import GHC.Driver.Env.Types (HscEnv (hsc_interp))
+import GHC.Driver.Main (hscParseStmtWithLocation)
 import GHC.Runtime.Interpreter.Types (Interp (interpInstance), InterpInstance (..))
 import GHCSpecter.Channel.Outbound.Types (ConsoleReply (..))
 
 runGhciOnGhc :: HscEnv -> IO ConsoleReply
 runGhciOnGhc env = do
+  let reply :: (Monad m) => Text -> m ConsoleReply
+      reply = pure . ConsoleReplyText (Just "ghci")
   let minterp = hsc_interp env
   case minterp of
-    Nothing ->
-      pure (ConsoleReplyText (Just "ghci") "no-interpreter")
+    Nothing -> reply "no-interpreter"
     Just interp ->
       case interpInstance interp of
-        ExternalInterp {} ->
-          pure (ConsoleReplyText (Just "ghci") "external interpreter is not supported.")
+        ExternalInterp {} -> reply "external interpreter is not supported."
         InternalInterp {} ->
           runInteractiveHsc env $ do
-            pure (ConsoleReplyText (Just "ghci") "I am inside interpreter")
+            maybe_stmt <- hscParseStmtWithLocation "<interactive>" 1 "3 + 4"
+            case maybe_stmt of
+              Nothing -> reply "parse failure: 3 + 4"
+              Just stmt -> reply "success"
